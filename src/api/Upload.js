e430bcc1980351778b5618587f9447cfe6da3f63
Khanh Huyen Nguyen <77928796+huyen010@users.noreply.github.com>
2022-12-29 10:15:06 +0700

    commit

--- src/api/Upload.js
+++ src/api/Upload.js
@@ -1,12 +1,13 @@
 import axiosClient from "./axiosClient";
 const UploadAPI = {
-    uploadFile(file){
-        const url = `http://188.166.245.252:3002/api/v1/web/upload-image`;
-        return axiosClient.post(url,file,{
-            headers: {
-              'content-type': 'multipart/form-data'
-            }});
-    }
-}
+  uploadFile(file) {
+    const url = `/api/v1/web/upload-image`;
+    return axiosClient.post(url, file, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+  },
+};
 
 export default UploadAPI;
